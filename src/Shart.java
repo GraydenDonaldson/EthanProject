55719ac3aafda60f3230af9a3e680f35758014f9
ethan <ethangat725@gmail.com>
2025-03-16 17:25:06 -0400

    shart

---
+++ src/Shart.java
@@ -0,0 +1,10 @@
+public class Shart {
+    public static void shart() {
+        while (true) {
+            System.out.print("shart ");
+        }
+    }
+    public static void main(String [] args) {
+        shart();
+    }
+}
